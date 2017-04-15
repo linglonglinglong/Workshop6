3ddd1838e00223b3ec16aefe78a9bae98c4ce864
Yueying Liu <648339108@qq.com>
2017-04-15 12:03:06 -0400

    database

--- server/src/database.js
+++ server/src/database.js
@@ -1,3 +1,5 @@
+//I've read the section "Migrate Data to Mock Database on Server"
+
 // Data goes here.
 var initialData = {
   // The "user" collection. Contains all of the users in our Facebook system.
