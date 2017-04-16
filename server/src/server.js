6cc764aa3b6cde35828eb136a077ceb5045760a6
Yueying Liu <648339108@qq.com>
2017-04-15 21:36:31 -0400

    failbook

--- server/src/server.js
+++ server/src/server.js
@@ -3,10 +3,6 @@ var express = require('express');
 // Creates an Express server.
 var app = express();
 
-// var TestModule = require('./util');
-// var reverseString = TestModule.reverseString;
-// var bodyParser = require('body-parser');
-// app.use(bodyParser.text());
 var database = require('./database');
 var readDocument = database.readDocument;
 
@@ -14,29 +10,10 @@ var readDocument = database.readDocument;
 // '..' means "go up one directory", so this translates into `client/build`!
 app.use(express.static('../client/build'));
 
-// Defines what happens when it receives the `GET /` request
-// app.get('/', function (req, res) {
-//   res.send('Hello World!');
-// });
-
 // Starts the server on port 3000!
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
-
-// Handle POST /reverse [data]
-// app.post('/reverse', function (req, res) {
-//   // If the request came with text, then the text() middleware handled it
-//   // and made `req.body` a string.
-//   // Check that req.body is a string.
-//   if (typeof(req.body) === 'string') {
-//     var reversed = reverseString(req.body);
-//     res.send(reversed);
-//   } else {
-//     // POST did not contain a string. Send an error code back!
-//     res.status(400).end()
-//   }
-// });
 
 /**
  * Resolves a feed item. Internal to the server, since it's synchronous.
